a8443a4d0be1b61263a07fc36a0f3411185fc2c5
Allen Chou <f1470891079@gmail.com>
2025-02-11 23:28:57 +0800

    Enhance import management with eslint-plugin-import
    - Add eslint-plugin-import for improved import statement linting
    - Configure import order rules with predefined groups
    - Enable alphabetical sorting of import statements
    - Standardize import organization across the project

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 
 export default [
@@ -6,5 +7,28 @@ export default [
   reactRecommended,
   {
     files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      // Explicitly register the "import" plugin
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
   },
 ];
